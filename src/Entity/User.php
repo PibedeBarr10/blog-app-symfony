d625eab26c3194492cd262b4c88b29d2450bff35
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2021-05-26 16:33:08 +0200

    make migrations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -51,6 +51,12 @@ class User implements UserInterface
      */
     private $likes;
 
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $emptyfield;
+
+
     public function __construct()
     {
         $this->favouritePost = new ArrayCollection();
