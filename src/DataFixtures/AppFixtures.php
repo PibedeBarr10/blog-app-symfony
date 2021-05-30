5cf563b7f0fe1ee45f4763780d7d15e8aa7fe677
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2021-05-31 00:31:45 +0200

    new migration

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,22 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-		$pass = "123";
+		//$pass = "123";
 		
-        $admin = new User();
-		$admin->setEmail("admin@email.com");
-		$admin->setPassword(
-			$passwordEncoder->encodePassword($admin, $pass)
-		);
-		$admin->setUsername("Admin");
-		$admin->setRoles(['ROLE_USER']);
-        $manager->persist($admin);
+        //$admin = new User();
+		//$admin->setEmail("admin@email.com");
+		//$admin->setPassword(
+		//	$passwordEncoder->encodePassword($admin, $pass)
+		//);
+		//$admin->setUsername("Admin");
+		//$admin->setRoles(['ROLE_USER']);
+        //$manager->persist($admin);
 
-        $manager->flush();
+        //$manager->flush();
     }
 }
